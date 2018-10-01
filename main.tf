a4987bb0aa21162263fe55afbc44378443e89380
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2018-10-01 09:33:34 +0300

    Using specific version of terraform-generic-label

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_region" "default" {
 
 # Define composite variables for resources
 module "label" {
-  source     = "git::https://github.com/huksley/terraform-generic-label.git?ref=master"
+  source     = "git::https://github.com/huksley/terraform-generic-label.git?ref=1.0_GA"
   namespace  = "${var.namespace}"
   name       = "${var.name}"
   stage      = "${var.stage}"
